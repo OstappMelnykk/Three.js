084a85f31752b2e6da9d37e8c58f4e59a8d1487a
OstappMelnykk <79ostap@ukr.net>
2024-12-27 17:57:16 +0200

    Create component canvas-box

---
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './canvas-box.component.html',
+  styleUrl: './canvas-box.component.scss'
+})
+export class CanvasBoxComponent {
+
+}
