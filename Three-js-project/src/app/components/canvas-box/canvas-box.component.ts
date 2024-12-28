17031cf93b3a147ac9644824a536880628d8ecaf
OstappMelnykk <79ostap@ukr.net>
2024-12-29 00:21:56 +0200

    Delete All and add some new logic
    Add:
    - axesHelper
    - gridHelper
    - box
    - ambientLight
    
    GUI:
    box.rotation: x
    box.rotation: y
    box.rotation: z

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -10,20 +10,22 @@ import * as dat from 'dat.gui'
   templateUrl: './canvas-box.component.html',
   styleUrl: './canvas-box.component.scss'
 })
-export class CanvasBoxComponent implements OnInit {
-  ngOnInit(): void {
+
+
+export class CanvasBoxComponent implements OnInit
+{
+  ngOnInit(): void
+  {
     this.createThreeJsBox();
   }
 
   createThreeJsBox(): void
   {
     const renderer = new THREE.WebGLRenderer()
-
     renderer.shadowMap.enabled = true;
-
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xFFEA00);
     document.body.appendChild(renderer.domElement);
+
 
     const scene = new THREE.Scene();
 
@@ -33,114 +35,66 @@ export class CanvasBoxComponent implements OnInit {
       0.1,
       1000
     );
+    camera.position.set(5, 5, 5)
+
     const orbit = new OrbitControls( camera, renderer.domElement );
-    camera.position.set(-5, 15, 15)
-    orbit.update();
 
-    const axesHelper = new THREE.AxesHelper(5)
-    scene.add(axesHelper)
+    const axesHelper = new THREE.AxesHelper(50)
+    const gridHelper = new THREE.GridHelper(30, 30)
+    gridHelper.position.set(0, -2, 0)
+    scene.add(gridHelper, axesHelper)
 
-    const textureloader = new THREE.TextureLoader();
+
 
     const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-    const boxMaterial = new THREE.MeshBasicMaterial({
-      //02olor: 0x00FF00,
-      //wireframe: true
-      map: textureloader.load('/assets/imgs/wall_texture.jpg'),
+    const boxMaterial = new THREE.MeshStandardMaterial({
+      color: 0xFFFFFF,
     })
-
-
     const box = new THREE.Mesh(boxGeometry, boxMaterial)
     scene.add(box)
-    box.castShadow = true
-    box.position.y = 2
 
-    const planeGeometry = new THREE.PlaneGeometry(30, 30)
-    const planeMaterial = new THREE.MeshStandardMaterial({
-      color: 0xFFFFFF,
-      side: THREE.DoubleSide
-    })
 
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial)
-    scene.add(plane)
-    plane.rotation.x = -Math.PI / 2;
-    plane.receiveShadow = true
 
-    const gridHelper = new THREE.GridHelper(30, 10)
-    scene.add(gridHelper)
-
-    const sphereGeometry = new THREE.SphereGeometry(4, 50, 50)
-    const sphereMaterial = new THREE.MeshStandardMaterial({
-      color: 0x0000FF,
-      wireframe: true
-    })
-    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-    scene.add(sphere)
-
-    sphere.position.set(-10, 10, 0)
-    sphere.castShadow = true
-
-    const ambientLight = new THREE.AmbientLight(0x333333);
+    const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
     scene.add(ambientLight);
-
-    const spotLight = new THREE.SpotLight(0xFFFFFF, 100);
-    spotLight.angle = 0.04
-    scene.add(spotLight);
-    spotLight.position.set(-10, 10, 0);
-    spotLight.castShadow = true;
-    spotLight.angle = 0.2;
-
-    const sLightHelper = new THREE.SpotLightHelper(spotLight);
-    scene.add(sLightHelper);
-
 
 
     const gui = new dat.GUI();
+    const cubeFolder = gui.addFolder('Cube')
+    cubeFolder.add(box.rotation, 'x', 0, Math.PI * 2)
+    cubeFolder.add(box.rotation, 'y', 0, Math.PI * 2)
+    cubeFolder.add(box.rotation, 'z', 0, Math.PI * 2)
+    cubeFolder.open()
 
+
+/*
     const options = {
-      sphereColor: "#ffea00",
-      wireframe: true,
-      speed: 0.01,
       angle: 0.2,
       penumbra: 0,
       intensity: 1,
     }
 
-    gui.addColor(options, 'sphereColor')
-      .onChange(function (e){
-        sphere.material.color.set(e)
-      })
-
-    gui.add(options, 'wireframe').onChange(function (e){
-      sphere.material.wireframe = e
-    })
-
-    gui.add(options, 'speed', 0, 0.1)
-
     gui.add(options, 'angle', 0, 1)
     gui.add(options, 'penumbra', 0, 1)
     gui.add(options, 'intensity', 0, 500)
+*/
 
 
-    let step = 0
+
+    window.addEventListener('resize', () => {
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+    })
+
+
 
     function animate(){
-      box.rotation.x += 0.002;
-      box.rotation.y += 0.002;
-      box.rotation.z += 0.002;
-
-      step += options.speed
-      sphere.position.y = 10 * Math.abs(Math.sin(step))
-
-      spotLight.angle = options.angle
-      spotLight.penumbra = options.penumbra
-      spotLight.intensity = options.intensity
-
-      sLightHelper.update()
-
-      renderer.render(scene, camera);
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
     }
 
-    renderer.setAnimationLoop(animate);
+    animate()
+
   }
 }
