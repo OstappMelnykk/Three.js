ba62301b3f7a272e8adbe8a807e0e4fba56539b2
OstappMelnykk <79ostap@ukr.net>
2024-12-29 05:32:54 +0200

    Perspective camera

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -26,11 +26,21 @@ export class CanvasBoxComponent implements OnInit
     const scene = new THREE.Scene()
     //scene.background = new THREE.Color(0xffffff);
     //scene.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png');
-    scene.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
-    scene.backgroundBlurriness = 0.5
+    //scene.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+    //scene.backgroundBlurriness = 0.5
 
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.z = 1.5
+    scene.add(new THREE.GridHelper)
+
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.0001,
+      1000)
+    camera.position.set(0, 2, 1)
+    camera.lookAt(0, 0.5, 0)
+
+
 
     const renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
@@ -42,12 +52,13 @@ export class CanvasBoxComponent implements OnInit
       renderer.setSize(window.innerWidth, window.innerHeight)
     })
 
-    new OrbitControls(camera, renderer.domElement)
+    let controls = new OrbitControls(camera, renderer.domElement)
 
     const geometry = new THREE.BoxGeometry()
     const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
     const cube = new THREE.Mesh(geometry, material)
+    cube.position.set(0, 0.5, 0)
     scene.add(cube)
 
     const stats = new Stats()
@@ -61,6 +72,24 @@ export class CanvasBoxComponent implements OnInit
     cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2)
     cubeFolder.open()
 
+    const cameraFolder = gui.addFolder("Camera")
+    cameraFolder.add(camera.position, "x", -10, 10)
+    cameraFolder.add(camera.position, "y", -10, 10)
+    cameraFolder.add(camera.position, "z", -10, 10)
+    cameraFolder.add(camera, "fov", 0, 180, 0.01).onChange(function () {
+      camera.updateProjectionMatrix()
+    })
+    cameraFolder.add(camera, "aspect", 0.00001, 10).onChange(function () {
+      camera.updateProjectionMatrix()
+    })
+    cameraFolder.add(camera, "near", 0.01, 10).onChange(function () {
+      camera.updateProjectionMatrix()
+    })
+    cameraFolder.add(camera, "far", 0.01, 10).onChange(function () {
+      camera.updateProjectionMatrix()
+    })
+    cameraFolder.open()
+
 
     function animate() {
       requestAnimationFrame(animate)
@@ -68,6 +97,7 @@ export class CanvasBoxComponent implements OnInit
       //cube.rotation.x += 0.01
       //cube.rotation.y += 0.01
 
+      camera.lookAt(0, 0.5, 0)
       renderer.render(scene, camera)
 
       stats.update()
