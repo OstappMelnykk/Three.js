e255adb92f8f8779aebc67d4d46b6495cbf52942
OstappMelnykk <79ostap@ukr.net>
2024-12-29 20:39:16 +0200

    Object3D
    Here are the most common things you can do with an Object3D:
        - Rotation
        - Position
        - Scale
        - Visibility
    
    Aslo avoids using a custom <canvas> tag and generates it by default:
    
    const renderer = new THREE.WebGLRenderer()
    renderer.setSize(window.innerWidth, window.innerHeight)
    document.body.appendChild(renderer.domElement)

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -24,29 +24,22 @@ export class CanvasBoxComponent implements OnInit
   createThreeJsBox(): void
   {
     const scene = new THREE.Scene()
+    scene.add(new THREE.AxesHelper(5))
 
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement
-    const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
-    renderer.setSize(canvas.clientWidth, canvas.clientHeight)
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.set(1, 2, 3)
 
-
-    const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
-    camera.position.z = 1.5
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
 
     window.addEventListener('resize', () => {
       camera.aspect = window.innerWidth / window.innerHeight
       camera.updateProjectionMatrix()
       renderer.setSize(window.innerWidth, window.innerHeight)
-      renderer.render(scene, camera)
     })
 
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.addEventListener("change", function(event: any) {
-      renderer.render(scene, camera)
-    })
-    const stats = new Stats()
-    document.body.appendChild(stats.dom)
-
+    new OrbitControls(camera, renderer.domElement)
 
     const geometry = new THREE.BoxGeometry()
     const material = new THREE.MeshNormalMaterial({ wireframe: true })
@@ -54,13 +47,41 @@ export class CanvasBoxComponent implements OnInit
     const cube = new THREE.Mesh(geometry, material)
     scene.add(cube)
 
+    const stats = new Stats()
+    document.body.appendChild(stats.dom)
 
-    function animate(){
+    const gui = new GUI()
+
+    const cubeFolder = gui.addFolder('Cube')
+    cubeFolder.add(cube, 'visible')
+    cubeFolder.open()
+
+    const positionFolder = cubeFolder.addFolder('Position')
+    positionFolder.add(cube.position, 'x', -5, 5)
+    positionFolder.add(cube.position, 'y', -5, 5)
+    positionFolder.add(cube.position, 'z', -5, 5)
+    positionFolder.open()
+
+    const rotationFolder = cubeFolder.addFolder('Rotation')
+    rotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+    rotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+    rotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+    rotationFolder.open()
+
+    const scaleFolder = cubeFolder.addFolder('Scale')
+    scaleFolder.add(cube.scale, 'x', -5, 5)
+    scaleFolder.add(cube.scale, 'y', -5, 5)
+    scaleFolder.add(cube.scale, 'z', -5, 5)
+    scaleFolder.open()
+
+    function animate() {
       requestAnimationFrame(animate)
+
+      renderer.render(scene, camera)
+
       stats.update()
     }
 
     animate()
-    renderer.render(scene, camera)
   }
 }
