b9f1f47e87fd1f13ae5cba95aa1e2043e5b71f0f
OstappMelnykk <79ostap@ukr.net>
2024-12-29 02:43:12 +0200

    Three.js Tutorials. Create Course Boilerplate

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -21,65 +21,14 @@ export class CanvasBoxComponent implements OnInit
 
   createThreeJsBox(): void
   {
+    const scene = new THREE.Scene()
+
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.z = 1.5
+
     const renderer = new THREE.WebGLRenderer()
-    renderer.shadowMap.enabled = true;
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.set(5, 5, 5)
-
-    const orbit = new OrbitControls( camera, renderer.domElement );
-
-    const axesHelper = new THREE.AxesHelper(50)
-    const gridHelper = new THREE.GridHelper(30, 30)
-    gridHelper.position.set(0, -2, 0)
-    scene.add(gridHelper, axesHelper)
-
-
-
-    const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-    const boxMaterial = new THREE.MeshStandardMaterial({
-      color: 0xFFFFFF,
-    })
-    const box = new THREE.Mesh(boxGeometry, boxMaterial)
-    scene.add(box)
-
-
-
-    const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
-    scene.add(ambientLight);
-
-
-    const gui = new dat.GUI();
-    const cubeFolder = gui.addFolder('Cube')
-    cubeFolder.add(box.rotation, 'x', 0, Math.PI * 2)
-    cubeFolder.add(box.rotation, 'y', 0, Math.PI * 2)
-    cubeFolder.add(box.rotation, 'z', 0, Math.PI * 2)
-    cubeFolder.open()
-
-
-/*
-    const options = {
-      angle: 0.2,
-      penumbra: 0,
-      intensity: 1,
-    }
-
-    gui.add(options, 'angle', 0, 1)
-    gui.add(options, 'penumbra', 0, 1)
-    gui.add(options, 'intensity', 0, 500)
-*/
-
-
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
 
     window.addEventListener('resize', () => {
       camera.aspect = window.innerWidth / window.innerHeight
@@ -87,10 +36,18 @@ export class CanvasBoxComponent implements OnInit
       renderer.setSize(window.innerWidth, window.innerHeight)
     })
 
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
 
-    function animate(){
+    function animate() {
       requestAnimationFrame(animate)
+
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+
       renderer.render(scene, camera)
     }
 
