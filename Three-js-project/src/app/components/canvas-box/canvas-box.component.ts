02bbecc981caf62783963bf843dd7809f0ac0492
OstappMelnykk <79ostap@ukr.net>
2024-12-30 20:19:08 +0200

    ExtrudeGeometry

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -50,38 +50,28 @@ export class CanvasBoxComponent implements OnInit
 
 
 
+// Створення форми (наприклад, прямокутник)
+    const shape = new THREE.Shape();
+    shape.moveTo(0, 0);
+    shape.lineTo(0, 1);
+    shape.lineTo(1, 1);
+    shape.lineTo(1, 0);
+    shape.lineTo(0, 0);
 
+// Налаштування екструзії
+    const extrudeSettings = {
+      steps: 2,
+      depth: 1,
+      bevelEnabled: true,
+      bevelThickness: 0.2,
+      bevelSize: 0.1,
+      bevelOffset: 0,
+      bevelSegments: 1,
+    };
 
-
-
-    // Create lights
-    const ambientLight = new THREE.AmbientLight(0x404040, 1);  // Soft white light
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);  // Main directional light (e.g., sunlight)
-    directionalLight.position.set(5, 10, 7);  // Adjust position for better illumination
-    scene.add(directionalLight);
-
-    const pointLight = new THREE.PointLight(0xff0000, 1, 10);  // Red point light
-    pointLight.position.set(0, 2, 3);
-    scene.add(pointLight);
-
-
-
-
-    // Створення форми (наприклад, серце)
-    const heartShape = new THREE.Shape();
-    heartShape.moveTo(5, 5);
-    heartShape.bezierCurveTo(5, 5, 4, 0, 0, 0);
-    heartShape.bezierCurveTo(-6, 0, -6, 7, -6, 7);
-    heartShape.bezierCurveTo(-6, 11, -3, 15.4, 5, 19);
-    heartShape.bezierCurveTo(12, 15.4, 16, 11, 16, 7);
-    heartShape.bezierCurveTo(16, 7, 16, 0, 10, 0);
-    heartShape.bezierCurveTo(7, 0, 5, 5, 5, 5);
-
-// Створення геометрії з форми
-    const geometry = new THREE.ShapeGeometry(heartShape);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
+// Створення геометрії з екструзії
+    const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+    const material = new THREE.MeshNormalMaterial({  });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
