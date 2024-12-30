3ea57914d27bc2e4b671f968290c1cc9baed68ad
OstappMelnykk <79ostap@ukr.net>
2024-12-30 06:44:15 +0200

    BufferGeometryLoader
    BufferGeometryLoader in Three.js is a class used to load geometry stored in a buffer format (for example, JSON or other supported formats) and convert it to a BufferGeometry object that can be used for rendering in a scene

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -33,178 +33,24 @@ export class CanvasBoxComponent implements OnInit
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     camera.position.set(1, 2, 3)
 
-    const renderer = new THREE.WebGLRenderer()
+    const renderer = new THREE.WebGLRenderer({ antialias: true})
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
 
     new OrbitControls(camera, renderer.domElement)
 
-//////////////////////////////////////////////////////////////////
-
-    //build in geometry
-
-    // const cube = new THREE.Mesh(
-    //   new THREE.BoxGeometry(),
-    //   new THREE.MeshNormalMaterial({ wireframe: true })
-    // )
-    //
-    // scene.add(cube)
-
-//////////////////////////////////////////////////////////////////
-
-    //cusom line geometry
-
-    /*const points = [
-      new THREE.Vector3(-1, 0, 0),
-      new THREE.Vector3(1, 0, 0),
-    ]
-
-    let geometry = new THREE.BufferGeometry().setFromPoints(points)
-
-    let line = new THREE.Line(
-      geometry,
-      new THREE.LineBasicMaterial({
-        color: 0x888888
-      }))
-
-    line.position.set(0, 1, 1)
-    scene.add(line)
-    */
-
-//////////////////////////////////////////////////////////////////
-
-    // const geometry = new THREE.BufferGeometry();
-    //
-    // const vertices = new Float32Array( [
-    //   -1.0, -1.0,  1.0, // v0
-    //   1.0, -1.0,  1.0, // v1
-    //   1.0,  1.0,  1.0, // v2
-    //
-    //   1.0,  1.0,  1.0, // v3
-    //   -1.0,  1.0,  1.0, // v4
-    //   -1.0, -1.0,  1.0  // v5
-    // ] );
-    //
-    // geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-    //
-    // const mesh = new THREE.Mesh(
-    //   geometry,
-    //   new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true }));
 
 
-//////////////////////////////////////////////////////////////////
+    const loader = new THREE.BufferGeometryLoader();
 
-
-    // const geometry = new THREE.BufferGeometry();
-    //
-    // const vertices = new Float32Array( [
-    //   -1.0, -1.0,  1.0, // v0
-    //   1.0, -1.0,  1.0, // v1
-    //   1.0,  1.0,  1.0, // v2
-    //   -1.0,  1.0,  1.0, // v3
-    // ] );
-    //
-    // const indices = [
-    //   0, 1, 2,
-    //   2, 3, 0,
-    // ];
-    //
-    // geometry.setIndex( indices );
-    // geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-    //
-    // const material = new THREE.MeshBasicMaterial( {
-    //   color: 0xff0000,
-    //   wireframe: true
-    // });
-    // const mesh = new THREE.Mesh( geometry, material );
-    //
-    //
-    // scene.add( mesh );
-
-//////////////////////////////////////////////////////////////////
-
-
-//     //додавання кольорів до вершин
-//
-//
-//     const vertices = new Float32Array([
-//       0, 1, 0,   // Верхня вершина
-//       -1, -1, 0,  // Ліва нижня вершина
-//       1, -1, 0   // Права нижня вершина
-//     ]);
-//
-// // Кольори
-//     const colors = new Float32Array([
-//       1, 0, 0,  // Червоний (для верхньої вершини)
-//       0, 1, 0,  // Зелений (для лівої вершини)
-//       0, 0, 1   // Синій (для правої вершини)
-//     ]);
-//
-// // Геометрія
-//     const geometry = new THREE.BufferGeometry();
-//     geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-//     geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-//
-// // Матеріал
-//     const material = new THREE.MeshBasicMaterial({
-//       vertexColors: true, // Використовувати кольори вершин
-//       side: THREE.DoubleSide
-//     });
-//
-// // Сітка
-//     const triangle = new THREE.Mesh(geometry, material);
-//     scene.add(triangle);
-
-
-
-//////////////////////////////////////////////////////////////////
-
-
-
-
-// Вершини п'ятикутника
-    const vertices = new Float32Array([
-      0, 1, 0,    // Верхня точка
-      -0.95, 0.31, 0, // Ліва верхня точка
-      -0.59, -0.81, 0, // Ліва нижня точка
-      0.59, -0.81, 0,  // Права нижня точка
-      0.95, 0.31, 0    // Права верхня точка
-    ]);
-
-// Індекси для створення трикутників
-    const indices = [
-      0, 1, 4, // Верхній трикутник
-      1, 2, 4, // Лівий нижній трикутник
-      2, 3, 4  // Правий нижній трикутник
-    ];
-
-    const colors = new Float32Array([
-      1, 0, 0,
-      0, 1, 0,
-      0, 0, 1,
-      0, 0.5, 0,
-      0.5, 0, 0,
-    ]);
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-    geometry.setIndex(indices);
-    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-
-    const material = new THREE.MeshBasicMaterial({
-      vertexColors: true,
-      side: THREE.DoubleSide
-    });
-
-// Сітка
-    const pentagon = new THREE.Mesh(geometry, material);
-    scene.add(pentagon);
-
-
-
-
-
-
+    loader.load(
+      '/assets/imgs/data.json',
+      function ( geometry ) {
+        const material = new THREE.MeshNormalMaterial({ wireframe: false, side: THREE.DoubleSide })
+        const object = new THREE.Mesh( geometry, material );
+        scene.add( object );
+      }
+    );
 
 
 
