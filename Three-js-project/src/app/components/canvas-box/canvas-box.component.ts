2100b8189534ee4443ec37c03e55182f89fcadd8
OstappMelnykk <79ostap@ukr.net>
2024-12-30 07:23:23 +0200

    ConvexGeometry
    ConvexGeometry is a geometry type in Three.js that allows you to create convex polygons from a set of given points. This type of geometry is used to build three-dimensional bodies from a set of points that are combined into a convex surface.

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -3,6 +3,8 @@ import * as THREE from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import Stats from "three/examples/jsm/libs/stats.module.js";
 import {GUI} from 'dat.gui'
+import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry.js'
+
 
 /*
     /addons/
@@ -28,7 +30,7 @@ export class CanvasBoxComponent implements OnInit
   createThreeJsBox(): void
   {
     const scene = new THREE.Scene()
-    scene.add(new THREE.AxesHelper(5))
+    scene.add(new THREE.AxesHelper(10))
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     camera.position.set(1, 2, 3)
@@ -41,16 +43,33 @@ export class CanvasBoxComponent implements OnInit
 
 
 
-    const loader = new THREE.BufferGeometryLoader();
+    // // Визначаємо набір точок для ConvexGeometry
+    // const points = [
+    //   new THREE.Vector3(-1, -1, 0),
+    //   new THREE.Vector3(1, -1, 0),
+    //   new THREE.Vector3(1, 1, 0),
+    //   new THREE.Vector3(-1, 1, 0),
+    //   new THREE.Vector3(0, 0, 2)
+    // ];
 
-    loader.load(
-      '/assets/imgs/data.json',
-      function ( geometry ) {
-        const material = new THREE.MeshNormalMaterial({ wireframe: false, side: THREE.DoubleSide })
-        const object = new THREE.Mesh( geometry, material );
-        scene.add( object );
-      }
-    );
+    const points = [];
+    const numberOfPoints = 100;  // кількість точок
+
+    for (let i = 0; i < numberOfPoints; i++) {
+      const x = Math.random() * 10 - 5;  // Генерація випадкового x від -5 до 5
+      const y = Math.random() * 10 - 5;  // Генерація випадкового y від -5 до 5
+      const z = Math.random() * 10 - 5;  // Генерація випадкового z від -5 до 5
+      points.push(new THREE.Vector3(x, y, z));
+    }
+
+    const geometry = new ConvexGeometry( points );
+    //const material = new THREE.MeshNormalMaterial({wireframe: true});
+    const material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
+    const mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+
+
 
 
 
