6c8ee1562eeed127c61abdb61fa6198254bda003
OstappMelnykk <79ostap@ukr.net>
2024-12-30 10:49:28 +0200

    BufferGeometry 1

--- Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
+++ Three-js-project/src/app/components/canvas-box/canvas-box.component.ts
@@ -44,55 +44,44 @@ export class CanvasBoxComponent implements OnInit
 
 
 
+    const material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
+    const geometry = new THREE.BufferGeometry()
+    const points = [
+      new THREE.Vector3(-1, 1, -1), //c
+      new THREE.Vector3(-1, -1, 1), //b
+      new THREE.Vector3(1, 1, 1), //a
+
+      new THREE.Vector3(1, 1, 1), //a
+      new THREE.Vector3(1, -1, -1), //d
+      new THREE.Vector3(-1, 1, -1), //c
+
+      new THREE.Vector3(-1, -1, 1), //b
+      new THREE.Vector3(1, -1, -1), //d
+      new THREE.Vector3(1, 1, 1), //a
+
+      new THREE.Vector3(-1, 1, -1), //c
+      new THREE.Vector3(1, -1, -1), //d
+      new THREE.Vector3(-1, -1, 1) //b
+    ]
+
+    geometry.setFromPoints(points)
+    geometry.computeVertexNormals()
+
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
 
 
-    const vertices = new Float32Array([
-      -1, -1, 0,
-      1, -1, 0,
-      1, 1, 0,
-      1, 1, 1,
-    ]);
 
-    const indices = [
-      0, 1,
-      1, 2,
-      2, 3,
-    ];
-
-    const geometry : THREE.BufferGeometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-    geometry.setIndex( indices );
-
-    const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
-
-    const lines = new THREE.Line(geometry, material);
-    lines.position.set(0,0,-1);
-    scene.add(lines);
-
-
-    const gui = new GUI();
-
-    let pointsFolders = [];
-
-    for (let i = 0; i < vertices.length / 3; i++) {
-      const point = {
-        x: vertices[i * 3],
-        y: vertices[i * 3 + 1],
-        z: vertices[i * 3 + 2]
-      };
-
-      const folder = gui.addFolder(`Point ${i + 1}`);
-      pointsFolders.push(folder);
-
-      let position = geometry.attributes['position']
-
-      folder.add(point, "x", -10, 10).onChange(() => { vertices[i * 3] = point.x; position.needsUpdate = true; });
-      folder.add(point, "y", -10, 10).onChange(() => { vertices[i * 3 + 1] = point.y; position.needsUpdate = true;});
-      folder.add(point, "z", -10, 10).onChange(() => { vertices[i * 3 + 2] = point.z; position.needsUpdate = true; });
-      folder.open()
+    const data = {
+      x: 1
     }
 
-
+    const gui = new GUI()
+    gui.add(data, 'x', -5, -1, 0.01).onChange(() => {
+      geometry.attributes['position'].array[3] = data.x
+      geometry.attributes['position'].needsUpdate = true
+    })
+    gui.open()
 
 
 
